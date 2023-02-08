f78c86ff91bc2ebabc1dcef3a92628bebd06143e
petar-cvit <petar.cvitanovic@gmail.com>
2023-02-08 11:46:09 +0100

    by color env var

--- config/services/namespace-a/service-b/main.go
+++ config/services/namespace-a/service-b/main.go
@@ -3,11 +3,18 @@ package main
 import "petar-cvit/cdk8s-argo/lib/models"
 
 func GetConfig(color string) models.AppProps {
+	specificByColor := map[string]string{
+		"green":  "red",
+		"blue":   "orange",
+		"orange": "blue",
+	}
+
 	return models.AppProps{
 		Env: map[string]string{
-			"COLOR":   color,
-			"TENANCY": "tenancy/" + color,
-			"KEY":     "im.service.b",
+			"COLOR":      color,
+			"TENANCY":    "tenancy/" + color,
+			"KEY":        "im.service.b",
+			"COMPLEMENT": specificByColor[color],
 		},
 		Replicas: 1,
 	}
