7b318747a8e057bf5eebdf2dc1edda91c763f33c
petar-cvit <petar.cvitanovic@gmail.com>
2023-03-06 13:38:07 +0100

    use v4

--- config/services/namespace-a/service-a/main.go
+++ config/services/namespace-a/service-a/main.go
@@ -2,13 +2,13 @@ package main
 
 import "petar-cvit/cdk8s-argo/lib/models"
 
-func GetConfig(color string) models.AppProps {
+func GetConfig(target string) models.AppProps {
 	return models.AppProps{
 		Env: map[string]string{
-			"COLOR":   color,
-			"TENANCY": "tenancy/" + color,
+			"TARGET":      target,
+			"KAFKA_TOPIC": target + "-offer-kafka-topic",
 		},
-		Host:     "https://colors.com/" + color,
+		Host:     "https://offer.api/" + target,
 		Replicas: 3,
 	}
 }
