1007428365408c14dda6541dd9f92865d732eb4f
petar-cvit <petar.cvitanovic@gmail.com>
2023-03-20 15:27:38 +0100

    trigger

--- config/services/demo/demo-service/main.go
+++ config/services/demo/demo-service/main.go
@@ -5,9 +5,10 @@ import "petar-cvit/cdk8s-argo/lib/models"
 func GetConfig(color string) models.AppProps {
 	return models.AppProps{
 		Env: map[string]string{
-			"COLOR":      color,
-			"TENANCY":    "tenancy/" + color,
-			"KAFKA_TOPI": color + ".rifgjealf",
+			"COLOR":       color,
+			"TENANCY":     "tenancy/" + color,
+			"KAFKA_TOPI":  color + ".rifgjealf",
+			"KAFKA_TOPIC": color + ".rifgjealf",
 		},
 		Replicas: 5,
 	}
