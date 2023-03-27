56376ca2c908f339b67805155af5d56ab9aa0cd1
petar-cvit <petar.cvitanovic@gmail.com>
2023-03-27 10:54:30 +0200

    host

--- config/services/demo/demo-service/main.go
+++ config/services/demo/demo-service/main.go
@@ -4,6 +4,8 @@ import "petar-cvit/cdk8s-argo/pipelines/models"
 
 func GetConfig(color string) models.AppProps {
 	return models.AppProps{
+		Name:      "demo-service",
+		Namespace: "demo",
 		Env: map[string]string{
 			"COLOR":       color,
 			"TENANCY":     "tenancy/" + color,
@@ -11,5 +13,6 @@ func GetConfig(color string) models.AppProps {
 			"KAFKA_TOPIC": color + ".rifgjealf",
 		},
 		Replicas: 5,
+		Host:     "https://my-demo.com",
 	}
 }
