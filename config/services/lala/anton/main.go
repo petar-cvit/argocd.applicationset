a29e74dc73cbefe94f250f90bfd084f99f77de3f
petar-cvit <petar.cvitanovic@gmail.com>
2023-03-28 13:39:52 +0200

    anton

---
+++ config/services/lala/anton/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "petar-cvit/cdk8s-argo/pipelines/models"
+
+func GetConfig(color string) models.AppProps {
+	specificByColor := map[string]string{
+		"green":  "red",
+		"blue":   "orange",
+		"orange": "blue",
+	}
+
+	return models.AppProps{
+		Name:      "anton",
+		Namespace: "lala",
+		Env: map[string]string{
+			"COLOR":      color,
+			"TENANCY":    "tenancy/" + color,
+			"KEY":        "im.service.b",
+			"COMPLEMENT": specificByColor[color],
+		},
+		Replicas: 1,
+	}
+}
